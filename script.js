042a38d041f8ef1a2ad956071abc94b020b925a2
ADITYA-PROGRAMMER5 <adityajadhav5211@gmail.com>
2026-09-16 17:32:57 +0530

    implement core functionality for adding numbers to display

---
+++ script.js
@@ -0,0 +1,30 @@
+const display = document.getElementById("display");
+
+let firstNumber = null;
+let operator = null;
+let waitingForSecondNumber = false;
+
+
+function addNumber(number) {
+
+    if (display.value === "Error") {
+        clearDisplay();
+    }
+
+    if (waitingForSecondNumber) {
+
+        display.value = number;
+
+        waitingForSecondNumber = false;
+
+    } 
+    else {
+
+        if (display.value === "0") {
+            display.value = number;
+        } 
+        else {
+            display.value += number;
+        }
+    }
+}
